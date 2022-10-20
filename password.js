118f6b76f55501a496900b5ac8e6b2cf3d03b33e
Heiikam <83463398+Heiikam@users.noreply.github.com>
2022-10-19 18:33:00 -0700

    Update password.js

--- password.js
+++ password.js
@@ -1,1 +1,13 @@
+var password = "auwff";
 
+function passcheck() {
+
+  if(document.getElementById('pass1').value != password) {
+    alert('wrong password, try again!');
+  }
+  
+  
+  if(document.getElementById('pass1').value == password) {
+    alert('Correct Password. Click OK!');
+  }
+}
