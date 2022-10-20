eef3564a1816eb529e732a26ea4adf696fba40a1
Heiikam <83463398+Heiikam@users.noreply.github.com>
2022-10-19 18:19:07 -0700

    Update password.js

--- password.js
+++ password.js
@@ -1,13 +1,1 @@
-var password = "1234";
 
-function passcheck() {
-	
-if(document.getElementById('pass1').value != password) {
-alert('Incorrect Password');
-	return false;
-}
-
-if(document.getElementById('pass1').value == password) {
-alert('Correct, Now click the ok button to continue');
-}
-}
